8c2778201af94e3ff0bd24ea7a91034020234a48
ChristopherJ123 <christopherz2002@hotmail.com>
2024-11-21 22:37:21 +0700

    testing

--- Testing/TestPerjalanan.py
+++ Testing/TestPerjalanan.py
@@ -2,8 +2,16 @@ dict1 = {'A': None, 'B': 'A', 'C': 'A', 'D': 'A', 'E': 'B', 'F': 'E', 'G': 'C', 
 destination = ['H']
 
 while dict1[destination[-1]] is not None:
+    print(destination)
+    print("destination[-1] is " + destination[-1])
     print("dict1[destination[-1]] is " + str(dict1[destination[-1]]))
+    print("---")
     destination.append(dict1[destination[-1]])
 destination.reverse()
 
+print("===")
 print(destination)
+print("===")
+
+print("Dari ", end="")
+print(" pergi ke ".join(destination))
