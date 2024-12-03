de6fd4274e97008f478171a4fcb784a4dc9c0f95
MARCO <marcoalexandertejo@gmail.com>
2024-12-03 21:57:18 +0700

    Surabaya graph

---
+++ Surabaya.py
@@ -0,0 +1,12 @@
+from Graph import Graph
+from Path import Path
+from Vertex import Vertex
+from Walking import Walking
+from Car import Car
+from Motorcycle import Motorcycle
+
+gr = Graph()
+car = Car(50, 10)
+motor = Motorcycle(40, 10)
+
+pakuwon_mall = Vertex(0,0,"Pakuwon mall")
