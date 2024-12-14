0083f1a4cffd472eca6727973e6dadba444183fb
Ein <142294432+ChristopherLouisT@users.noreply.github.com>
2024-12-14 17:11:48 +0700

    Ubah kapasitas Bensin sesuai RL

--- Motorcycle.py
+++ Motorcycle.py
@@ -4,4 +4,4 @@ class Motorcycle(Transportation):
     def __init__(self, speed, fuel_efficiency):
         super().__init__(speed, {1, 3}, fuel_efficiency)
         self.vehicle_type = "Motorcycle"
-        self.current_fuel = 100
+        self.current_fuel = 5 #Liter
