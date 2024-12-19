ff2e38237b007e109ee09e9c153b0b4b6adc3e6f
Ein <142294432+ChristopherLouisT@users.noreply.github.com>
2024-12-19 17:30:40 +0700

    Update Bicycle.py

--- Bicycle.py
+++ Bicycle.py
@@ -2,5 +2,5 @@ from Transportation import Transportation
 
 class Bicycle(Transportation):
     def __init__(self, speed):
-       super().__init__(speed, {1, 3})
+       super().__init__(speed, {1, 3}, 0)
        self.vehicle_type = "Bicycle"
