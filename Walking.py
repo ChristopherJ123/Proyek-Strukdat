4f0e785c8ef51fb5487567d14f36956fa2876d44
Ein <142294432+ChristopherLouisT@users.noreply.github.com>
2024-12-19 17:31:01 +0700

    Update Walking.py

--- Walking.py
+++ Walking.py
@@ -2,5 +2,5 @@ from Transportation import Transportation
 
 class Walking(Transportation):
     def __init__(self, speed):
-        super().__init__(speed, {1, 3, 4})
+        super().__init__(speed, {1, 3, 4}, 0)
         self.vehicle_type = "Walking"
